6787dc280ea31d020092627c6fa6c075b4b4ca1a
YoungJae970304 <sksdl1643020@gmail.com>
2024-07-03 18:58:15 +0900

    07.03.home
    집에서 수정
    플레이어 FSM 유기
    차지 공격 구현

--- Assets/02_Scripts/PlayerController.cs
+++ Assets/02_Scripts/PlayerController.cs
@@ -1,5 +1,165 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1f;
+    public float jumpForce = 5f;
+    private float chargeTime = 0f;
+    private float chargeAtk = 1f; // 기본 공격과 강공격의 경계
+    private float fullChargeAtk = 2f; // 강공격과 특수공격의 경계
+
+    public bool isGrounded = true;
+    private bool isCharging = false;
+
+    private Rigidbody2D rig2d;
+    private Animator anim;
+    private SpriteRenderer sr;
+    private BoxCollider2D boxCol;
+
+    
+    
+
+    private void Start()
+    {
+        rig2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+        boxCol = GetComponent<BoxCollider2D>();
+
+
+    }
+
+    private void Update()
+    {
+        HandleInput();
+        UpdateCharge();
+    }
+
+    void HandleInput()
+    {
+        float moveInput = Input.GetAxisRaw("Horizontal");
+
+        if (moveInput != 0)
+        {
+            Move(moveInput);
+        }
+        else
+        {
+            Idle();
+        }
+
+        if (Input.GetKey(KeyCode.S) && isGrounded)
+        {
+            Crouch();
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            Jump();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCharge();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            ChargeAttack();
+        }
+    }
+
+    void Move(float moveInput)
+    {
+        rig2d.velocity = new Vector2(moveInput * speed, rig2d.velocity.y);
+
+        sr.flipX = moveInput < 0;
+    }
+
+    void Idle()
+    {
+        Debug.Log("대기");
+        boxCol.enabled = true;
+    }
+
+    void Crouch()
+    {
+        Debug.Log("앉기");
+        rig2d.velocity = Vector2.zero;
+        boxCol.enabled = false;
+    }
+
+    void Jump()
+    {
+        isGrounded = false;
+        rig2d.AddForce(new Vector2(rig2d.velocity.x, jumpForce), ForceMode2D.Impulse);
+    }
+
+    void StartCharge()
+    {
+        isCharging = true;
+        chargeTime = 0f;
+    }
+
+    void ChargeAttack()
+    {
+        isCharging = false;
+
+        if (chargeTime >= fullChargeAtk)
+        {
+            FullCAttack();
+        }
+        else if (chargeTime >= chargeAtk)
+        {
+            CAttack();
+        }
+        else
+        {
+            BasicAttack();
+        }
+
+        chargeTime = 0f;
+    }
+
+    void UpdateCharge()
+    {
+        if (isCharging)
+        {
+            chargeTime += Time.deltaTime;
+        }
+    }
+
+    void BasicAttack()
+    {
+        Debug.Log("기본 공격");
+    }
+
+    void CAttack()
+    {
+        Debug.Log("차지 공격");
+    }
+
+    void FullCAttack()
+    {
+        Debug.Log("풀차지 공격");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
+
+
+/*
+using System.Collections;
+using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -10,6 +170,7 @@ public enum PlayerState
     Crouch,
     Jump,
     // 공격도 애니메이션 블렌드로 gauge값에 따라? 그러면 Attack 하나만 있어도 됨
+    Attack,
     WeekAttack,
     MiddleAttack,
     StrongAttack,
@@ -28,7 +189,7 @@ public class PlayerController : MonoBehaviour
     BoxCollider2D boxCol;
 
     float speed = 1f;
-    float jumpForce = 250f;
+    float jumpForce = 5f;
     float lastMoveDir;
 
     bool isGrounded = true;
@@ -47,8 +208,6 @@ public class PlayerController : MonoBehaviour
     
     void FixedUpdate()
     {
-        NowStateJudge();
-
         switch (playerState)
         {
             case PlayerState.Idle:
@@ -64,14 +223,8 @@ public class PlayerController : MonoBehaviour
             case PlayerState.Jump:
                 JumpState();
                 break;
-            case PlayerState.WeekAttack:
-                
-                break;
-            case PlayerState.MiddleAttack:
-                
-                break;
-            case PlayerState.StrongAttack:
-                
+            case PlayerState.Attack:
+                Attack();
                 break;
             case PlayerState.Hitted:
                 
@@ -85,18 +238,25 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void NowStateJudge()
+    private void Update()
     {
-        if (!Input.anyKey)
+        NowStateCheck();
+    }
+
+    void NowStateCheck()
+    {
+        if (Input.GetMouseButtonDown(0))
         {
-            playerState = PlayerState.Idle;
+            playerState = PlayerState.Attack;
         }
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            playerState = PlayerState.Run;
+        }
+
         if (isGrounded)
         {
-            if (Input.GetAxis("Horizontal") != 0)
-            {
-                playerState = PlayerState.Run;
-            }
             if (Input.GetKey(KeyCode.S))
             {
                 playerState = PlayerState.Crouch;
@@ -107,6 +267,12 @@ public class PlayerController : MonoBehaviour
             }
         }
         
+
+        if (!Input.anyKey)
+        {
+            playerState = PlayerState.Idle;
+        }
+
     }
 
     void IdleState()
@@ -117,7 +283,7 @@ public class PlayerController : MonoBehaviour
 
     void RunState()
     {
-        Debug.Log("달기기");
+        Debug.Log("달리기");
         float moveInput = Input.GetAxisRaw("Horizontal");
 
         rig2d.velocity = new Vector2(moveInput * speed, rig2d.velocity.y);
@@ -141,15 +307,25 @@ public class PlayerController : MonoBehaviour
         Debug.Log("점프");
         isGrounded = false;
 
-        rig2d.AddForce(new Vector2(rig2d.velocity.x, jumpForce));
+        rig2d.AddForce(new Vector2(rig2d.velocity.x, jumpForce), ForceMode2D.Impulse);
         playerState = PlayerState.Run;
+    }
+
+    void Attack()
+    {
+        Debug.Log("공격");
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            isGrounded = true;
+            if (collision.contacts[0].normal.y >= 0.7 || collision.contacts[0].normal.x >= 1 || collision.contacts[0].normal.x >= -1)
+            {
+                Debug.Log("경사면 초기화");
+                isGrounded = true;
+            }
         }
     }
 }
+*/
