183b32f45dae4638aecea898fb7221fe0328a14d
Mintchoco_Y <sksdl1643020@gmail.com>
2024-12-13 12:34:39 +0900

    빌드되게 수정

--- Assets/02_Scripts/Managers/UIManager.cs
+++ Assets/02_Scripts/Managers/UIManager.cs
@@ -49,7 +49,11 @@ public class UIManager : MonoBehaviour
 
     public void BTN_Quit()
     {
+#if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit(); // 어플리케이션 종료
+#endif
     }
 
     public void OpenOverPanel()
